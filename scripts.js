14a9bd5699f60f4e4ea40ca60a608dc2d974248e
Sakthivel Saravanan <sakthivels@infoservices.com>
2025-11-14 14:46:01 +0530

    added new changes

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("message").innerText =
+  "This text was changed by script.js";
+
+function showAlert() {
+  alert("Hello! Your static site is working perfectly on Vercel 🎉");
+}
